ee40bc2ea84aa952459f214d67da9d4cdb1ec87f
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-01-30 10:49:52 +0000

    feat: implement GraphAPI service

---
+++ KsApi/models/graphql/adapters/GraphAPI.BackingState+BackingStateTests.swift
@@ -0,0 +1,12 @@
+import GraphAPI
+@testable import KsApi
+import XCTest
+
+final class GraphAPI_BackingState_BackingStateTests: XCTestCase {
+  func testErroredState_Success() {
+    XCTAssertEqual(
+      GraphAPI.BackingState(rawValue: "errored"),
+      GraphAPI.BackingState.from(BackingState.errored)
+    )
+  }
+}
