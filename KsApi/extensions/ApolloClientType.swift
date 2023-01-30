c8a05afa459f71e5e1d525e941ebfd6d6421f85e
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-01-30 10:49:52 +0000

    feat: scaffold project architecture

---
+++ KsApi/extensions/ApolloClientType.swift
@@ -0,0 +1,7 @@
+import ApolloAPI
+import ReactiveSwift
+
+public protocol ApolloClientType {
+  func fetch<Query: GraphQLQuery>(query: Query) -> SignalProducer<Query.Data, ErrorEnvelope>
+  func perform<Mutation: GraphQLMutation>(mutation: Mutation) -> SignalProducer<Mutation.Data, ErrorEnvelope>
+}
