e59efe0871c88826f94de23445fb0d08f2b27cdc
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-05-19 08:25:52 +0000

    feat: enhance application features

---
+++ .fastlane/beta-changelog.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'yaml'
+require 'plist'
+require 'aws-sdk-s3'
+
+#
+# Configure AWS SDK to use the system trust store provided in CI to avoid SSL
+# verification failures when talking to S3. Fall back to the macOS system paths
+# used on CircleCI if the env vars are not present.
+#
+ca_bundle = ENV['SSL_CERT_FILE'].to_s.empty? ? '/etc/ssl/cert.pem' : ENV['SSL_CERT_FILE']
+ca_dir = ENV['SSL_CERT_DIR'].to_s.empty? ? '/etc/ssl/certs' : ENV['SSL_CERT_DIR']
+Aws.config.update(
+  ssl_ca_bundle: ca_bundle,
+  ssl_ca_directory: ca_dir
+)
+ENV['AWS_CA_BUNDLE'] ||= ca_bundle
+
+#
+# Interfacing with the builds bucket on S3
+#
+
+def bucket_name
+  'ios-ksr-builds'
+end
+
+def bucket
+   @bucket ||= Aws::S3::Bucket.new(bucket_name)
+end
+
+def current_builds
+  @current_builds ||= YAML::load(bucket.object('builds.yaml').get.body.read)
+end
+
+#
+# Script
+#
+
+previous_changelog = current_builds.last['changelog']
+current_changelog_from_fastlane = File.read('.FASTLANE_RELEASE_NOTES.tmp')
+
+file_name = '.RELEASE_NOTES.tmp'
+File.delete(file_name) if File.exist?(file_name)
+File.open(file_name, 'w') do |f|
+  f.write(current_changelog_from_fastlane)
+  f.write(previous_changelog
+    .split("\n")
+    .map {|line| "# #{line}"}
+    .join("\n")
+  )
+end
---
+++ Library/UILabel+SimpleHTMLTests.swift
@@ -0,0 +1,16 @@
+@testable import Library
+import XCTest
+
+final class UILabelSimpleHTMLTests: XCTestCase {
+  // FIXME: MBL-2857
+  func testSetHTML() {
+    let label = UILabel()
+    label.textColor = .red
+    label.textAlignment = .center
+    label.setHTML("<b>Howdy<b> there!")
+
+    XCTAssertEqual(label.text, "Howdy there!")
+    XCTAssertEqual(label.textColor, UIColor.red)
+    XCTAssertEqual(label.textAlignment, NSTextAlignment.center)
+  }
+}
