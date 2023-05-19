e32637f0ab95135dedf0a4bccd434caaa630f1c1
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-05-19 08:25:52 +0000

    feat: add 5 feature implementations

---
+++ KsApi/models/ClientSecretEnvelope.swift
@@ -0,0 +1,19 @@
+import Foundation
+import GraphAPI
+import ReactiveSwift
+
+public struct ClientSecretEnvelope: Decodable {
+  public let clientSecret: String
+}
+
+// MARK: - GraphQL Adapters
+
+extension ClientSecretEnvelope {
+  static func envelopeProducer(from data: GraphAPI.CreateSetupIntentMutation.Data)
+    -> SignalProducer<ClientSecretEnvelope, ErrorEnvelope> {
+    guard let envelope = ClientSecretEnvelope.clientSecretEnvelope(from: data) else {
+      return SignalProducer(error: .couldNotParseJSON)
+    }
+    return SignalProducer(value: envelope)
+  }
+}
