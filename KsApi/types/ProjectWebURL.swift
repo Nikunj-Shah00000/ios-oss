468976d733e257900b3a0315df0c11afc09f9ec9
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-05-19 08:25:52 +0000

    feat: implement ksapi functionality

---
+++ KsApi/types/ProjectWebURL.swift
@@ -0,0 +1,17 @@
+import GraphAPI
+
+public protocol HasProjectWebURL {
+  var projectWebURL: String { get }
+}
+
+extension Project: HasProjectWebURL {
+  public var projectWebURL: String {
+    self.urls.web.project
+  }
+}
+
+extension GraphAPI.ProjectFragment: HasProjectWebURL {
+  public var projectWebURL: String {
+    self.url
+  }
+}
