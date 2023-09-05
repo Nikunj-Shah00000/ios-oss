754b097a00d7e96c1ac59d83fb83e2db661d0e46
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-09-05 06:01:52 +0000

    feat: extend module capabilities

---
+++ Library/LoginIntent.swift
@@ -0,0 +1,34 @@
+public enum LoginIntent: String {
+  case activity
+  case backProject
+  case discoveryOnboarding
+  case erroredPledge
+  case generic
+  case loginTab
+  case messageCreator
+  case onboarding
+  case starProject
+
+  var trackingString: String {
+    switch self {
+    case .activity:
+      return "activity"
+    case .backProject:
+      return "pledge"
+    case .discoveryOnboarding:
+      return "discovery_prompt"
+    case .erroredPledge:
+      return "errored_pledge"
+    case .generic:
+      return "generic"
+    case .loginTab:
+      return "login_tab"
+    case .messageCreator:
+      return "message_creator"
+    case .onboarding:
+      return "onboarding"
+    case .starProject:
+      return "star"
+    }
+  }
+}
