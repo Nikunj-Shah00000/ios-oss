f42dae6859d3f678cfa4df2e6ad09c171ff498f8
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-09-05 06:01:52 +0000

    feat(graphapi): extend module capabilities

---
+++ GraphAPI/Sources/Schema/Enums/PaymentIncrementState.graphql.swift
@@ -0,0 +1,25 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public enum PaymentIncrementState: String, EnumType {
+  /// The initial state of the payment increment; payment source has not been charged
+  case unattempted = "UNATTEMPTED"
+  /// Payment source was successfully charged
+  case collected = "COLLECTED"
+  /// Payment source could not be charged due to an errored payment source or authentication being required
+  case errored = "ERRORED"
+  /// Payment increment is cancelled by user action or is an abandoned increment due to failure to complete payment
+  case cancelled = "CANCELLED"
+  /// Backer issued a dispute and we (kickstarter) lost the dispute
+  case chargebackLost = "CHARGEBACK_LOST"
+  /// Payment increment has been refunded
+  case refunded = "REFUNDED"
+  /// Payment increment amount has been adjusted due to refund and is unattempted
+  case unattemptedAdjusted = "UNATTEMPTED_ADJUSTED"
+  /// Payment increment amount has been adjusted due to refund and is collected
+  case collectedAdjusted = "COLLECTED_ADJUSTED"
+  /// Payment increment amount has been adjusted due to refund and is cancelled
+  case cancelledAdjusted = "CANCELLED_ADJUSTED"
+}
