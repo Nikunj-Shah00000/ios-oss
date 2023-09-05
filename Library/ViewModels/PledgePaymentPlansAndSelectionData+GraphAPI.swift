b23c00d2d4bdc5ee14c5b4413807721d95d82e94
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-09-05 06:01:52 +0000

    feat: enhance application features

---
+++ Library/ViewModels/PledgePaymentPlansAndSelectionData+GraphAPI.swift
@@ -0,0 +1,24 @@
+import GraphAPI
+import KsApi
+
+extension PledgePaymentPlansAndSelectionData {
+  public init(
+    withPaymentPlanFragment paymentPlan: GraphAPI.BuildPaymentPlanQuery.Data.Project.PaymentPlan,
+    selectedPlan: PledgePaymentPlansType,
+    project: Project
+  ) {
+    var increments: [PledgePaymentIncrement] = []
+
+    if let fetchedIncrements = paymentPlan.paymentIncrements {
+      increments = fetchedIncrements
+        .compactMap { PledgePaymentIncrement(withGraphQLFragment: $0.fragments.paymentIncrementFragment) }
+    }
+
+    self.init(
+      selectedPlan: selectedPlan,
+      increments: increments,
+      ineligible: !paymentPlan.amountIsPledgeOverTimeEligible,
+      project: project
+    )
+  }
+}
