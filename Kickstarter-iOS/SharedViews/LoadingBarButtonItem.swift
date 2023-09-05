9018ee4dfaa377bfd870207d7f50f78db99504ef
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-09-05 06:01:52 +0000

    feat: implement kickstarter-ios functionality

---
+++ Kickstarter-iOS/SharedViews/LoadingBarButtonItem.swift
@@ -0,0 +1,32 @@
+import KDS
+import Library
+import SwiftUI
+
+struct LoadingBarButtonItem: View {
+  @Binding var saveEnabled: Bool
+  @Binding var showLoading: Bool
+  let titleText: String
+  let action: () -> Void
+
+  var body: some View {
+    let buttonColor = self.$saveEnabled.wrappedValue ? LegacyColors.ksr_create_700
+      .swiftUIColor() : LegacyColors.ksr_create_300.swiftUIColor()
+
+    HStack {
+      if !self.showLoading {
+        Button(self.titleText) {
+          self.showLoading = true
+          self.action()
+        }
+        .font(Font(UIFont.ksr_body()))
+        .foregroundColor(buttonColor)
+        .disabled(!self.$saveEnabled.wrappedValue)
+      } else {
+        ProgressView()
+          .foregroundColor(LegacyColors.ksr_support_700.swiftUIColor())
+      }
+    }
+    .accessibilityElement(children: .combine)
+    .accessibilityLabel(self.titleText)
+  }
+}
