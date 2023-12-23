a46e60365cf86dc591d97ed15f82396b7506a1b8
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-12-23 03:37:52 +0000

    feat(style): add responsive styles to TableViewStyles

---
+++ Library/Styles/TableViewStyles.swift
@@ -0,0 +1,37 @@
+import Foundation
+import KDS
+import KsApi
+import Prelude
+import Prelude_UIKit
+import UIKit
+
+public func baseTableControllerStyle<TVC: UITableViewControllerProtocol>
+(estimatedRowHeight: CGFloat = 44.0) -> ((TVC) -> TVC) {
+  let style = baseControllerStyle()
+    <> TVC.lens.view.backgroundColor .~ LegacyColors.ksr_white.uiColor()
+    <> TVC.lens.tableView.rowHeight .~ UITableView.automaticDimension
+    <> TVC.lens.tableView.estimatedRowHeight .~ estimatedRowHeight
+
+  return style <> TVC.lens.tableView.separatorStyle .~ .none
+}
+
+public func baseTableViewCellStyle<TVC: UITableViewCellProtocol>() -> ((TVC) -> TVC) {
+  return
+    TVC.lens.contentView.layoutMargins %~~ { _, cell in
+      if cell.traitCollection.isRegularRegular {
+        return .init(topBottom: Styles.grid(3), leftRight: Styles.grid(12))
+      }
+      return .init(topBottom: Styles.grid(1), leftRight: Styles.grid(2))
+    }
+    <> TVC.lens.backgroundColor .~ LegacyColors.ksr_white.uiColor()
+    <> (TVC.lens.contentView .. UIView.lens.preservesSuperviewLayoutMargins) .~ false
+    <> TVC.lens.layoutMargins .~ .init(all: 0.0)
+    <> TVC.lens.preservesSuperviewLayoutMargins .~ false
+    <> TVC.lens.selectionStyle .~ .none
+}
+
+public let baseTableViewCellTitleLabelStyle: LabelStyle = { label in
+  label
+    |> \.textColor .~ LegacyColors.ksr_support_700.uiColor()
+    |> \.font .~ .ksr_body()
+}
