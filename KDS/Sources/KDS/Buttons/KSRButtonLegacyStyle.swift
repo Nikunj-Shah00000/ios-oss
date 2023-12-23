84aa1296e63f8fcc8cb5ba26bdaf0f5ef98a918c
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-12-23 03:37:52 +0000

    feat: implement feature functionality

---
+++ KDS/Sources/KDS/Buttons/KSRButtonLegacyStyle.swift
@@ -0,0 +1,83 @@
+import UIKit
+
+/// Defines legacy button styles using the `KSRButtonStyleConfiguration` protocol.
+///
+/// These styles are used as fallback or for buttons that do not have equivalents in the new design system.
+public enum KSRButtonLegacyStyle: KSRButtonStyleConfiguration, CaseIterable {
+  case blue
+  case grey
+
+  public var backgroundColor: UIColor {
+    switch self {
+    case .blue: LegacyColors.Buttons.blue.uiColor()
+    case .grey: LegacyColors.ksr_support_300.uiColor()
+    }
+  }
+
+  public var highlightedBackgroundColor: UIColor {
+    switch self {
+    case .blue: LegacyColors.Buttons.blue.uiColor().mixDarker(0.36)
+    case .grey: LegacyColors.ksr_support_300.uiColor().mixDarker(0.36)
+    }
+  }
+
+  public var disabledBackgroundColor: UIColor {
+    switch self {
+    case .blue: LegacyColors.Buttons.blue.uiColor().mixLighter(0.36)
+    case .grey: LegacyColors.ksr_support_300.uiColor().mixLighter(0.12)
+    }
+  }
+
+  public var titleColor: UIColor {
+    switch self {
+    case .blue: LegacyColors.ksr_white.uiColor()
+    case .grey: LegacyColors.ksr_support_700.uiColor()
+    }
+  }
+
+  public var highlightedTitleColor: UIColor {
+    switch self {
+    case .blue: LegacyColors.ksr_white.uiColor()
+    case .grey: LegacyColors.ksr_support_700.uiColor()
+    }
+  }
+
+  public var disabledTitleColor: UIColor {
+    switch self {
+    case .blue: LegacyColors.ksr_white.uiColor()
+    case .grey: LegacyColors.ksr_support_400.uiColor()
+    }
+  }
+
+  public var font: UIFont {
+    .ksr_ButtonLabel()
+  }
+
+  public var borderWidth: CGFloat {
+    0.0
+  }
+
+  public var borderColor: UIColor {
+    .clear
+  }
+
+  public var highlightedBorderColor: UIColor {
+    self.borderColor
+  }
+
+  public var disabledBorderColor: UIColor {
+    self.borderColor
+  }
+
+  public var cornerRadius: CGFloat {
+    Dimension.CornerRadius.small
+  }
+
+  public var buttonConfiguration: UIButton.Configuration {
+    .filled()
+  }
+
+  public var loadingIndicatorColor: UIColor {
+    self.highlightedBackgroundColor
+  }
+}
---
+++ KDS/Sources/KDS/Colors/SemanticColor.swift
@@ -0,0 +1,79 @@
+import SwiftUI
+import UIKit
+
+public protocol AdaptiveColor {
+  /// Returns a dynamically-provided `UIColor`, which responds to light/dark mode.
+  var dynamicColor: UIColor { get }
+}
+
+public extension AdaptiveColor {
+  // UIColor with default opacity.
+  func uiColor() -> UIColor {
+    return self.dynamicColor
+  }
+
+  // Color with default opacity.
+  func swiftUIColor() -> Color {
+    Color(uiColor: self.uiColor())
+  }
+
+  // UIColor with custom opacity.
+  func uiColor(opacity alpha: CGFloat) -> UIColor {
+    return self.dynamicColor.withAlphaComponent(alpha)
+  }
+
+  // Color with custom opacity.
+  func swiftUIColor(opacity: CGFloat) -> Color {
+    Color(uiColor: self.uiColor(opacity: opacity))
+  }
+}
+
+/// A semantic color from the Kickstarter design system, like "surface/primary".
+/// Includes a light and dark mode color pair, as well as an identifying title.
+public struct SemanticColor: AdaptiveColor {
+  public let dynamicColor: UIColor
+
+  public let name: String
+
+  public init(_ name: String, lightMode: CoreColor, darkMode: CoreColor) {
+    self.init(name, lightMode: lightMode, lightModeAlpha: 1.0, darkMode: darkMode, darkModeAlpha: 1.0)
+  }
+
+  public init(
+    _ name: String,
+    lightMode: CoreColor,
+    lightModeAlpha: Double,
+    darkMode: CoreColor,
+    darkModeAlpha: Double
+  ) {
+    self.name = name
+
+    let lightModeColor = UIColor(coreColor: lightMode, alpha: lightModeAlpha)
+    let darkModeColor = UIColor(coreColor: darkMode, alpha: darkModeAlpha)
+
+    self.dynamicColor = UIColor { traits in
+      if traits.userInterfaceStyle == .dark {
+        return darkModeColor
+      } else {
+        return lightModeColor
+      }
+    }
+  }
+}
+
+/// Used for old design system colors which can't be mapped directly to the Kickstarter color palette.
+public struct LegacyColor: AdaptiveColor {
+  public let name: String
+  public let dynamicColor: UIColor
+
+  init(_ name: String, lightMode: UIColor, darkMode: UIColor) {
+    self.name = name
+    self.dynamicColor = UIColor { traits in
+      if traits.userInterfaceStyle == .dark {
+        return darkMode
+      } else {
+        return lightMode
+      }
+    }
+  }
+}
