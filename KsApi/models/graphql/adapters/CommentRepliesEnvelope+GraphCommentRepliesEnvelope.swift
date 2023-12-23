aba8797c16b83721f0c7baf35328a95460922fae
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2023-12-23 03:37:52 +0000

    feat(ksapi): extend module capabilities

---
+++ KsApi/models/graphql/adapters/CommentRepliesEnvelope+GraphCommentRepliesEnvelope.swift
@@ -0,0 +1,43 @@
+import Foundation
+import GraphAPI
+import ReactiveSwift
+
+extension CommentRepliesEnvelope {
+  /**
+   Returns a minimal `CommentRepliesEnvelope` from a `FetchCommentRepliesQuery.Data`
+   */
+  static func commentRepliesEnvelope(
+    from data: GraphAPI.FetchCommentRepliesQuery.Data
+  ) -> CommentRepliesEnvelope? {
+    guard let parentCommentFragment = data.comment?.asComment?.fragments.commentWithRepliesFragment.fragments
+      .commentBaseFragment,
+      let repliesData = data.comment?.asComment?.fragments.commentWithRepliesFragment.replies,
+      let parentComment = Comment.comment(from: parentCommentFragment, replyCount: repliesData.totalCount)
+    else {
+      return nil
+    }
+
+    let replies = repliesData.edges?
+      .compactMap { $0?.node?.fragments.commentFragment }
+      .compactMap {
+        Comment.comment(from: $0.fragments.commentBaseFragment, replyCount: $0.replies?.totalCount)
+      } ?? []
+
+    return CommentRepliesEnvelope(
+      comment: parentComment,
+      cursor: repliesData.pageInfo.startCursor,
+      hasPreviousPage: repliesData.pageInfo.hasPreviousPage,
+      replies: replies,
+      totalCount: repliesData.totalCount
+    )
+  }
+
+  static func envelopeProducer(
+    from data: GraphAPI.FetchCommentRepliesQuery
+      .Data
+  ) -> SignalProducer<CommentRepliesEnvelope, ErrorEnvelope> {
+    guard let envelope = CommentRepliesEnvelope.commentRepliesEnvelope(from: data) else { return .empty }
+
+    return SignalProducer(value: envelope)
+  }
+}
---
+++ KsApi/models/graphql/adapters/CommentsEnvelope+GraphCommentsEnvelope.swift
@@ -0,0 +1,44 @@
+import Foundation
+import GraphAPI
+
+extension CommentsEnvelope {
+  /**
+   Returns a minimal `CommentsEnvelope` from a `FetchProjectCommentsQuery.Data`
+   */
+  static func commentsEnvelope(from data: GraphAPI.FetchProjectCommentsQuery.Data) -> CommentsEnvelope? {
+    guard let comments = data.project?.comments?.edges?
+      .compactMap({ $0?.node?.fragments.commentFragment })
+      .compactMap({
+        Comment.comment(from: $0.fragments.commentBaseFragment, replyCount: $0.replies?.totalCount) })
+    else { return nil }
+
+    return CommentsEnvelope(
+      comments: comments,
+      cursor: data.project?.comments?.pageInfo.endCursor,
+      hasNextPage: data.project?.comments?.pageInfo.hasNextPage ?? false,
+      slug: data.project?.slug,
+      totalCount: data.project?.comments?.totalCount ?? 0,
+      updateID: nil
+    )
+  }
+
+  /**
+   Returns a minimal `CommentsEnvelope` from a `FetchUpdateCommentsQuery.Data`
+   */
+  static func commentsEnvelope(from data: GraphAPI.FetchUpdateCommentsQuery.Data) -> CommentsEnvelope? {
+    guard let comments = data.post?.asFreeformPost?.comments?.edges?
+      .compactMap({ $0?.node?.fragments.commentFragment })
+      .compactMap({
+        Comment.comment(from: $0.fragments.commentBaseFragment, replyCount: $0.replies?.totalCount) })
+    else { return nil }
+
+    return CommentsEnvelope(
+      comments: comments,
+      cursor: data.post?.asFreeformPost?.comments?.pageInfo.endCursor,
+      hasNextPage: data.post?.asFreeformPost?.comments?.pageInfo.hasNextPage ?? false,
+      slug: nil,
+      totalCount: data.post?.asFreeformPost?.comments?.totalCount ?? 0,
+      updateID: data.post?.asFreeformPost?.id
+    )
+  }
+}
