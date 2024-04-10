e39bdd06abfcacb3ab96828df39de3210e5500d2
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2024-04-10 01:13:52 +0000

    feat(kickstarter-ios): extend module capabilities

---
+++ Kickstarter-iOS/Features/SortFilter/HeaderView/SearchFiltersHeaderView.swift
@@ -0,0 +1,111 @@
+import KDS
+import Library
+import SwiftUI
+
+struct SelectedSearchFiltersHeaderView: View {
+  @ObservedObject var selectedFilters: SearchFilters
+  let didTapPill: (SearchFilterPill) -> Void
+
+  var body: some View {
+    SearchFiltersHeaderView(
+      didTapPill: self.didTapPill,
+      pills: self.selectedFilters.pills
+    )
+  }
+}
+
+struct SearchFiltersHeaderView: View {
+  let didTapPill: (SearchFilterPill) -> Void
+  let pills: [SearchFilterPill]
+
+  @SwiftUICore.Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
+  var body: some View {
+    ScrollView(.horizontal) {
+      HStack {
+        ForEach(self.pills) { pill in
+          switch pill.buttonType {
+          case let .image(image):
+            ImagePillButton(
+              action: {
+                self.didTapPill(pill)
+              },
+              image: image,
+              isHighlighted: pill.isHighlighted,
+              count: pill.count
+            )
+          case let .dropdown(title):
+            DropdownPillButton(
+              action: {
+                self.didTapPill(pill)
+              },
+              title: title,
+              isHighlighted: pill.isHighlighted,
+              count: pill.count
+            )
+          case let .toggle(title):
+            TogglePillButton(
+              action: {
+                self.didTapPill(pill)
+              },
+              title: title,
+              isHighlighted: pill.isHighlighted,
+              image: nil
+            )
+          case let .toggleWithImage(title, image):
+            TogglePillButton(
+              action: {
+                self.didTapPill(pill)
+              },
+              title: title,
+              isHighlighted: pill.isHighlighted,
+              image: image
+            )
+          }
+        }
+      }
+      .padding(EdgeInsets(
+        top: Styles.grid(3),
+        leading: self.horizontalSizeClass == .compact ?
+          Constants.pillLeftInsetForIPhone : Constants.pillLeftInsetForIPad,
+        bottom: Styles.grid(1),
+        trailing: Styles.grid(1)
+      ))
+    }
+    .scrollIndicators(.never)
+    .background(Colors.Background.Surface.primary.swiftUIColor())
+  }
+}
+
+private enum Constants {
+  // These match the card insets in BackerDashboardProjectCell
+  static let pillLeftInsetForIPad: CGFloat = Styles.grid(20)
+  static let pillLeftInsetForIPhone: CGFloat = Styles.grid(2)
+}
+
+let previewPills = [
+  SearchFilterPill(
+    isHighlighted: true,
+    filterType: .sort,
+    buttonType: .image(UIImage.checkmark)
+  ),
+  SearchFilterPill(
+    isHighlighted: false,
+    filterType: .category,
+    buttonType: .dropdown("Hello world")
+  ),
+  SearchFilterPill(
+    isHighlighted: false,
+    filterType: .recommended,
+    buttonType: .toggle("I'm a toggle")
+  ),
+  SearchFilterPill(
+    isHighlighted: true,
+    filterType: .projectsWeLove,
+    buttonType: .toggleWithImage("Switch me", UIImage.add)
+  )
+]
+
+#Preview {
+  SearchFiltersHeaderView(didTapPill: { _ in }, pills: previewPills)
+}
---
+++ Kickstarter-iOS/Features/SortFilter/LocationView/LocationView.swift
@@ -0,0 +1,127 @@
+import KDS
+import KsApi
+import Library
+import SwiftUI
+
+public struct LocationView: View {
+  let defaultLocations: [Location]
+  let suggestedLocations: [Location]
+  @Binding var selectedLocation: Location?
+  let onSearchedForLocations: (String) -> Void
+
+  @State var searchText: String = ""
+
+  func buttonLabel(title: String, isSelected: Bool) -> some View {
+    HStack(spacing: Constants.buttonLabelSpacing) {
+      RadioButton(isSelected: isSelected)
+      Text(title)
+        .font(InterFont.bodyLG.swiftUIFont())
+        .foregroundStyle(Colors.Text.primary.swiftUIColor())
+    }
+  }
+
+  @ViewBuilder var defaultLocationsList: some View {
+    VStack(alignment: .leading, spacing: Constants.spacing) {
+      Button {
+        self.selectedDefaultLocation(nil)
+      } label: {
+        self.buttonLabel(
+          title: Strings.Location_anywhere(),
+          isSelected: self.selectedLocation.isNil
+        )
+      }
+      ForEach(self.defaultLocations) { item in
+        Button {
+          self.selectedDefaultLocation(item)
+        } label: {
+          self.buttonLabel(
+            title: item.displayableName,
+            isSelected: self.selectedLocation?.id == item.id
+          )
+        }
+      }
+    }
+    .padding(Constants.padding)
+    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+  }
+
+  public var body: some View {
+    ScrollView {
+      self.defaultLocationsList
+    }
+    .searchable(
+      text: self.$searchText,
+      placement: .navigationBarDrawer(displayMode: .always),
+      prompt: Strings.Location_searchbox_placeholder()
+    )
+    .searchSuggestions {
+      ForEach(self.suggestedLocations) { location in
+        Text(location.displayableName)
+          .font(InterFont.bodyLG.swiftUIFont())
+          .foregroundStyle(Colors.Text.primary.swiftUIColor())
+          .searchCompletion(location.displayableName)
+      }
+    }
+    .onAppear {
+      self.searchText = self.selectedLocation?.displayableName ?? ""
+    }
+    .onChange(of: self.searchText) { newValue in
+      self.didChangeSearchText(newValue)
+    }
+    .onChange(of: self.selectedLocation) { newValue in
+      if newValue.isNil {
+        self.didResetLocation()
+      }
+    }
+  }
+
+  func didChangeSearchText(_ newValue: String) {
+    // If the search bar is already showing the displayableName
+    // of the selected location, no action is needed.
+    if let location = self.selectedLocation {
+      if location.displayableName == newValue {
+        return
+      }
+    }
+
+    // If the new search text is the displayableName of a suggested location,
+    // then they selected an option from auto-complete.
+    // Set the selected location.
+    if let location = self.suggestedLocations.first(where: { $0.displayableName == newValue }) {
+      self.selectedLocation = location
+      return
+    }
+
+    // Otherwise, they have some partial location text entered
+    // in the search bar.
+    // Update the suggested locations.
+    self.selectedLocation = nil
+    self.onSearchedForLocations(newValue)
+  }
+
+  func selectedDefaultLocation(_ location: Location?) {
+    self.selectedLocation = location
+
+    // If a location is selected from the default location list,
+    // update the search bar.
+    // This will also trigger `didChangeSearchText`.
+    self.searchText = self.selectedLocation?.displayableName ?? ""
+
+    // Clear the autosuggest results, too.
+    self.onSearchedForLocations("")
+  }
+
+  func didResetLocation() {
+    if !self.searchText.isEmpty {
+      self.searchText = ""
+    }
+  }
+
+  internal enum Constants {
+    static let padding = Spacing.unit_06
+    static let spacing = Spacing.unit_06
+    static let buttonLabelSpacing = Spacing.unit_02
+  }
+}
+
+extension KsApi.Location: @retroactive Identifiable {}
