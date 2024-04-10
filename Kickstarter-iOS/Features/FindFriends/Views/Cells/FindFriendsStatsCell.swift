582437acea8ae3ae980c78b41d4e3f568a23763a
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2024-04-10 01:13:52 +0000

    feat: enhance application features

---
+++ Kickstarter-iOS/Features/FindFriends/Views/Cells/FindFriendsStatsCell.swift
@@ -0,0 +1,89 @@
+import KDS
+import KsApi
+import Library
+import Prelude
+import ReactiveSwift
+import UIKit
+
+protocol FindFriendsStatsCellDelegate: AnyObject {
+  func findFriendsStatsCellShowFollowAllFriendsAlert(friendCount: Int)
+}
+
+internal final class FindFriendsStatsCell: UITableViewCell, ValueCell {
+  @IBOutlet fileprivate var backedProjectsLabel: UILabel!
+  @IBOutlet fileprivate var bulletSeparatorView: UIView!
+  @IBOutlet fileprivate var friendsLabel: UILabel!
+  @IBOutlet fileprivate var friendsCountLabel: UILabel!
+  @IBOutlet fileprivate var backedProjectsCountLabel: UILabel!
+  @IBOutlet fileprivate var followAllButton: UIButton!
+
+  internal weak var delegate: FindFriendsStatsCellDelegate?
+
+  fileprivate let viewModel: FindFriendsStatsCellViewModelType = FindFriendsStatsCellViewModel()
+
+  func configureWith(value: (stats: FriendStatsEnvelope, source: FriendsSource)) {
+    self.viewModel.inputs.configureWith(stats: value.stats, source: value.source)
+  }
+
+  override func bindViewModel() {
+    super.bindViewModel()
+
+    self.friendsCountLabel.rac.text = self.viewModel.outputs.friendsCountText
+
+    self.backedProjectsCountLabel.rac.text = self.viewModel.outputs.backedProjectsCountText
+
+    self.followAllButton.rac.hidden = self.viewModel.outputs.hideFollowAllButton
+
+    self.followAllButton.rac.title = self.viewModel.outputs.followAllText
+
+    self.viewModel.outputs.notifyDelegateShowFollowAllFriendsAlert
+      .observeForUI()
+      .observeValues { [weak self] count in
+        self?.delegate?.findFriendsStatsCellShowFollowAllFriendsAlert(friendCount: count)
+      }
+  }
+
+  override func bindStyles() {
+    super.bindStyles()
+
+    _ = self.friendsLabel
+      |> UILabel.lens.textColor .~ LegacyColors.ksr_support_400.uiColor()
+      |> UILabel.lens.font .~ .ksr_subhead()
+      |> UILabel.lens.text %~ { _ in Strings.social_following_stats_friends() }
+
+    _ = self.friendsCountLabel
+      |> UILabel.lens.textColor .~ LegacyColors.ksr_support_700.uiColor()
+      |> UILabel.lens.font .~ .ksr_title2()
+
+    _ = self.backedProjectsLabel
+      |> UILabel.lens.textColor .~ LegacyColors.ksr_support_400.uiColor()
+      |> UILabel.lens.font .~ .ksr_subhead()
+      |> UILabel.lens.text %~ { _ in Strings.social_following_stats_backed_projects() }
+
+    _ = self.self.backedProjectsCountLabel
+      |> UILabel.lens.textColor .~ LegacyColors.ksr_support_700.uiColor()
+      |> UILabel.lens.font .~ .ksr_title2()
+
+    self.followAllButton.applyStyleConfiguration(KSRButtonStyle.green)
+
+    _ = self.followAllButton
+      |> UIButton.lens.targets .~ [(self, action: #selector(self.followAllButtonTapped), .touchUpInside)]
+
+    _ = self.bulletSeparatorView
+      |> UIView.lens.backgroundColor .~ LegacyColors.ksr_support_300.uiColor()
+      |> UIView.lens.alpha .~ 0.7
+
+    _ = self
+      |> baseTableViewCellStyle()
+      |> UITableViewCell.lens.backgroundColor .~ LegacyColors.ksr_white.uiColor()
+      |> UITableViewCell.lens.contentView.layoutMargins %~~ { _, cell in
+        cell.traitCollection.isRegularRegular
+          ? .init(topBottom: Styles.grid(4), leftRight: Styles.grid(20))
+          : .init(all: Styles.grid(4))
+      }
+  }
+
+  @objc func followAllButtonTapped() {
+    self.viewModel.inputs.followAllButtonTapped()
+  }
+}
