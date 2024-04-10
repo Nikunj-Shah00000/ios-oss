5445833c714dc51ae77d093e01b64c00b4a7fef0
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2024-04-10 01:13:52 +0000

    feat: implement kickstarter-ios functionality

---
+++ Kickstarter-iOS/Features/SurveyResponse/Controller/PledgeManagerWebViewController.swift
@@ -0,0 +1,149 @@
+import KsApi
+import Library
+import Prelude
+import UIKit
+import WebKit
+
+internal protocol PledgeManagerWebViewControllerDelegate: AnyObject {
+  /// Called when the delegate should notify the parent that self was dismissed.
+  func pledgeManagerWebViewControllerDismissed()
+}
+
+internal final class PledgeManagerWebViewController: WebViewController {
+  internal weak var delegate: PledgeManagerWebViewControllerDelegate?
+  private var sessionStartedObserver: Any?
+  fileprivate let viewModel: PledgeManagerWebViewModelType = PledgeManagerWebViewModel()
+
+  internal static func configuredWith(url: String)
+    -> PledgeManagerWebViewController {
+    let vc = PledgeManagerWebViewController()
+    vc.viewModel.inputs.configureWith(url: url)
+    return vc
+  }
+
+  internal override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.navigationItem.leftBarButtonItem =
+      UIBarButtonItem(
+        title: Strings.general_navigation_buttons_close(),
+        style: .plain,
+        target: self,
+        action: #selector(self.closeButtonTapped)
+      )
+
+    self.viewModel.inputs.viewDidLoad()
+
+    self.viewModel.outputs.title
+      .observeForUI()
+      .observeValues { [weak self] title in
+        self?.title = title
+      }
+  }
+
+  deinit {
+    self.sessionStartedObserver.doIfSome(NotificationCenter.default.removeObserver)
+  }
+
+  internal override func bindViewModel() {
+    super.bindViewModel()
+
+    self.viewModel.outputs.dismissViewController
+      .observeForControllerAction()
+      .observeValues { [weak self] in
+        self?.navigationController?.dismiss(animated: true, completion: nil)
+        self?.delegate?.pledgeManagerWebViewControllerDismissed()
+      }
+
+    self.viewModel.outputs.goToLoginSignup
+      .observeForControllerAction()
+      .observeValues { [weak self] intent in
+        self?.goToLoginSignup(with: intent)
+      }
+
+    self.sessionStartedObserver = NotificationCenter.default
+      .addObserver(forName: .ksr_sessionStarted, object: nil, queue: nil) { [weak self] _ in
+        self?.viewModel.inputs.userSessionStarted()
+      }
+
+    self.viewModel.outputs.goToNativeScreen
+      .observeForControllerAction()
+      .observeValues { [weak self] (nativeNavigationRequest: PledgeManagerNativeNatigationRequest) in
+        switch nativeNavigationRequest {
+        case let .goToProject(param, refTag): self?.goToProject(param: param, refTag: refTag)
+        case let .goToPledge(param): self?.goToPledge(param: param)
+        case let .goToUpdate(param, updateId):
+          self?.viewModel.inputs.fetchUpdateVCData(param: param, updateId: updateId)
+        }
+      }
+
+    self.viewModel.outputs.presentUpdateVC
+      .observeForControllerAction()
+      .observeValues { [weak self] project, update in
+        self?.goToUpdate(project: project, update: update)
+      }
+
+    self.viewModel.outputs.webViewLoadRequest
+      .observeForControllerAction()
+      .observeValues { [weak self] request in
+        self?.webView.load(request)
+      }
+  }
+
+  @objc fileprivate func closeButtonTapped() {
+    self.viewModel.inputs.closeButtonTapped()
+  }
+
+  internal func webView(
+    _: WKWebView,
+    decidePolicyFor navigationAction: WKNavigationAction,
+    decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+  ) {
+    decisionHandler(
+      self.viewModel.inputs.decidePolicyFor(
+        navigationAction: WKNavigationActionData(navigationAction: navigationAction)
+      )
+    )
+  }
+
+  // MARK: - Handle login
+
+  fileprivate func goToLoginSignup(with intent: LoginIntent) {
+    let loginSignupViewController = LoginToutViewController.configuredWith(
+      loginIntent: intent
+    )
+    self.presentViewController(loginSignupViewController)
+  }
+
+  // MARK: - Deeplinks
+
+  fileprivate func goToProject(param: Param, refTag: RefTag?) {
+    let vc = ProjectPageViewController.configuredWith(
+      projectOrParam: .right(param),
+      refInfo: RefInfo(refTag)
+    )
+    self.presentViewController(vc)
+  }
+
+  fileprivate func goToUpdate(project: Project, update: Update) {
+    let vc = UpdateViewController.configuredWith(
+      project: project,
+      update: update,
+      context: .deepLink
+    )
+    self.presentViewController(vc)
+  }
+
+  fileprivate func goToPledge(param: Param) {
+    let vc = ManagePledgeViewController.instantiate()
+    vc.configureWith(params: (param, nil))
+    self.presentViewController(vc)
+  }
+
+  fileprivate func presentViewController(_ vc: UIViewController) {
+    let nav = NavigationController(rootViewController: vc)
+    nav.modalPresentationStyle = self.traitCollection.userInterfaceIdiom == .pad ? .fullScreen : .formSheet
+
+    self.present(nav, animated: true, completion: nil)
+  }
+}
