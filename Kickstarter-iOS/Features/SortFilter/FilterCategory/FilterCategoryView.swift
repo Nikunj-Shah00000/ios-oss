6e147d307b9eb34942ba4d7c38a0b23589af1179
Nikunj-Shah00000 <nikunjshah0310@gmail.com>
2024-04-10 01:13:52 +0000

    feat: add 5 feature implementations

---
+++ Kickstarter-iOS/Features/SortFilter/FilterCategory/FilterCategoryView.swift
@@ -0,0 +1,149 @@
+import KDS
+import KsApi
+import Library
+import SwiftUI
+
+struct FilterCategoryView: View {
+  let categories: [KsApi.Category]
+  @Binding var selectedCategory: SearchFiltersCategory
+
+  var body: some View {
+    VStack(spacing: 0) {
+      if self.categories.isEmpty {
+        ProgressView()
+          .controlSize(.large)
+          .progressViewStyle(CircularProgressViewStyle(tint: Colors.Icon.green.swiftUIColor()))
+          .padding(Constants.progressViewPadding)
+      } else {
+        self.categoryList
+      }
+    }
+    .background(Colors.Background.Surface.primary.swiftUIColor())
+  }
+
+  @ViewBuilder
+  private var categoryList: some View {
+    List(self.categories) { category in
+      VStack(spacing: 0) {
+        HStack {
+          Text(category.name)
+            .font(Font.ksr_headingLG())
+            .foregroundStyle(Colors.Text.primary.swiftUIColor())
+          Spacer()
+          self.radioButton(isSelected: self.selectedCategory.isRootCategorySelected(category))
+            .id("\(category.id)-radio-button")
+        }
+        .background(Colors.Background.Surface.primary.swiftUIColor())
+        .padding(.vertical, Constants.rowPaddingVertical)
+        .padding(.horizontal, Constants.rowPaddingHorizontal)
+
+        self.subcategories(
+          for: category
+        )
+
+        self.separator
+          .id("\(category.id)-separator")
+      }
+      .background(Colors.Background.Surface.primary.swiftUIColor())
+      .listRowInsets(EdgeInsets()) // Remove List internal insets
+      .listRowSeparator(.hidden) // Hide default separators
+      .contentShape(Rectangle())
+      .onTapGesture { () in
+        self.selectedCategory = .rootCategory(category)
+      }
+      .id(category.id)
+    }
+    .listStyle(.plain)
+  }
+
+  @ViewBuilder
+  private func subcategories(for category: KsApi.Category) -> some View {
+    if self.selectedCategory.isRootCategorySelected(category),
+       let subcategories = category.subcategories?.nodes {
+      FlowLayout(horizontalSpacing: 8, verticalSpacing: 8, alignment: .leading) {
+        TitlePillButton(
+          title: Strings.Project_status_all(),
+          isHighlighted: !self.selectedCategory.hasSubcategory(),
+          count: nil
+        ) {
+          self.selectedCategory = .rootCategory(category)
+        }
+        ForEach(subcategories) { subcategory in
+          TitlePillButton(
+            title: subcategory.name,
+            isHighlighted: self.selectedCategory.isSubcategorySelected(subcategory),
+            count: nil
+          ) {
+            self.selectedCategory = .subcategory(
+              rootCategory: category,
+              subcategory: subcategory
+            )
+          }
+          .id(subcategory.id)
+        }
+      }
+      .padding(EdgeInsets(top: 4, leading: 24, bottom: 16, trailing: 24))
+    }
+  }
+
+  @ViewBuilder
+  private var separator: some View {
+    Rectangle()
+      .fill(Colors.Border.subtle.swiftUIColor())
+      .frame(height: Constants.separatorHeight)
+      .frame(maxWidth: .infinity)
+  }
+
+  @ViewBuilder
+  private func radioButton(isSelected: Bool) -> some View {
+    ZStack {
+      Circle()
+        .strokeBorder(
+          isSelected ? Colors.Border.subtle.swiftUIColor() : Colors.Border.bold.swiftUIColor(),
+          lineWidth: Constants.radioButtonOuterBorder
+        )
+
+      if isSelected {
+        Circle()
+          .strokeBorder(
+            Colors.Background.selected.swiftUIColor(),
+            lineWidth: Constants.radioButtonInnerBorder
+          )
+      }
+    }
+    .frame(width: Constants.radioButtonSize, height: Constants.radioButtonSize)
+  }
+}
+
+private enum Constants {
+  static let headerPadding = Spacing.unit_06
+  static let progressViewPadding = Spacing.unit_06
+  static let radioButtonSize = Spacing.unit_06
+  static let radioButtonOuterBorder: CGFloat = 1.0
+  static let radioButtonInnerBorder = Spacing.unit_02
+  static let resetButtonMaxWidth: CGFloat = 130.0
+  static let rowPaddingHorizontal = Spacing.unit_06
+  static let rowPaddingVertical = Spacing.unit_05
+  static let separatorHeight: CGFloat = 1.0
+}
+
+extension KsApi.Category: @retroactive Identifiable {}
+
+#if targetEnvironment(simulator)
+
+  let previewCategories = (1...5).map { i in
+    Category(
+      analyticsName: nil,
+      id: "\(i)",
+      name: "Category Number \(i)"
+    )
+    /* TODO: add subcategories here for a nicer preview */
+  }
+
+  #Preview("Filter Categories") {
+    FilterCategoryView(
+      categories: previewCategories,
+      selectedCategory: .constant(.rootCategory(previewCategories[2]))
+    )
+  }
+#endif
---
+++ Kickstarter-iOS/Features/SortFilter/HeaderView/PillButtons.swift
@@ -0,0 +1,180 @@
+import KDS
+import Library
+import SwiftUI
+
+private let buttonFont = InterFont.headingMD
+
+internal struct SearchFiltersPillStyle: SwiftUI.ButtonStyle {
+  let isHighlighted: Bool
+  /// The pill style adds an extra half-unit of spacing to its label, which assumes that the label is text
+  /// and needs a little extra space in the end caps. If the pill is displaying square content (i.e. an icon),
+  /// set `isSquare` to `true` to remove that extra spacing.
+  var isSquare: Bool = false
+  private let padding: CGFloat = Spacing.unit_02
+  private let extraPadding: CGFloat = Spacing.unit_01
+
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .buttonStyle(.borderless)
+      .font(buttonFont.swiftUIFont())
+      .foregroundStyle(.tint)
+      .tint(
+        Colors.Text.primary.swiftUIColor()
+      )
+      .padding(
+        EdgeInsets(
+          top: self.padding,
+          leading: self.isSquare ? self.padding : (self.padding + self.extraPadding),
+          bottom: self.padding,
+          trailing: self.isSquare ? self.padding : (self.padding + self.extraPadding)
+        )
+      )
+      .clipShape(Capsule())
+      .overlay(
+        Capsule()
+          .stroke(
+            self.isHighlighted ? Colors.Border.active.swiftUIColor() : Colors.Border.bold.swiftUIColor(),
+            lineWidth: self.isHighlighted ? 2.0 : 1.0
+          )
+      )
+      .opacity(configuration.isPressed ? 0.5 : 1.0)
+  }
+}
+
+private struct ScaledToFontSize: ViewModifier {
+  let horizontal: Bool
+  let vertical: Bool
+  // We want pills to scale with the font size.
+  @ScaledMetric var scaledSize: CGFloat = buttonFont.defaultSize
+
+  func body(content: Content) -> some View {
+    content.frame(
+      width: self.horizontal ? self.scaledSize : nil,
+      height: self.vertical ? self.scaledSize : nil
+    )
+  }
+}
+
+private extension View {
+  /// Make sure all pills render at the same height, by clamping their content to a height scaled to the selected font size.
+  func pillHeight() -> some View {
+    modifier(ScaledToFontSize(horizontal: false, vertical: true))
+  }
+}
+
+internal struct FilterBadgeView: View {
+  let count: Int
+  var body: some View {
+    Text("\(self.count)")
+      .font(InterFont.headingXS.swiftUIFont())
+      .padding(EdgeInsets(
+        top: Spacing.unit_01,
+        leading: Spacing.unit_01,
+        bottom: Spacing.unit_01,
+        trailing: Spacing.unit_01
+      ))
+      .foregroundStyle(Colors.Text.primary.swiftUIColor())
+      .background(Colors.Background.Accent.Gray.subtle.swiftUIColor())
+      .clipShape(RoundedRectangle(cornerRadius: Styles.grid(1)))
+  }
+}
+
+internal struct TitlePillButton: View {
+  let title: String
+  let isHighlighted: Bool
+  let count: Int?
+  let action: () -> Void
+
+  var body: some View {
+    Button(action: self.action) {
+      HStack {
+        Text(self.title)
+        if let count = self.count, count > 0 {
+          FilterBadgeView(count: count)
+        }
+      }
+      .pillHeight()
+    }
+    .buttonStyle(SearchFiltersPillStyle(
+      isHighlighted: self.isHighlighted
+    ))
+  }
+}
+
+internal struct ImagePillButton: View {
+  let action: () -> Void
+  let image: UIImage
+  let isHighlighted: Bool
+  let count: Int
+
+  var body: some View {
+    Button(action: self.action) {
+      HStack {
+        Image(uiImage: self.image)
+          .aspectRatio(1.0, contentMode: .fill)
+          // Always render image pills as square, so the capsule becomes a circle.
+          .modifier(ScaledToFontSize(horizontal: true, vertical: true))
+        if self.count > 0 {
+          FilterBadgeView(count: self.count)
+        }
+      }
+      .pillHeight()
+    }
+    .buttonStyle(SearchFiltersPillStyle(
+      isHighlighted: self.isHighlighted,
+      isSquare: true
+    ))
+  }
+}
+
+internal struct DropdownPillButton: View {
+  let action: () -> Void
+  let title: String
+  let isHighlighted: Bool
+  let count: Int
+
+  var body: some View {
+    Button(action: self.action) {
+      HStack {
+        Text(self.title)
+        if self.count > 0 {
+          FilterBadgeView(count: self.count)
+        }
+        if let carat = Library.image(named: "arrow-down") {
+          Spacer()
+          Image(uiImage: carat)
+            .renderingMode(.template)
+            .aspectRatio(contentMode: .fill)
+        }
+      }
+      .pillHeight()
+    }
+    .buttonStyle(SearchFiltersPillStyle(
+      isHighlighted: self.isHighlighted
+    ))
+  }
+}
+
+internal struct TogglePillButton: View {
+  let action: () -> Void
+  let title: String
+  let isHighlighted: Bool
+  let image: UIImage?
+
+  var body: some View {
+    Button(action: self.action) {
+      HStack(spacing: Spacing.unit_01) {
+        if let image = self.image {
+          Image(uiImage: image)
+            .aspectRatio(1.0, contentMode: .fill)
+            .modifier(ScaledToFontSize(horizontal: false, vertical: true))
+        }
+        Text(self.title)
+      }
+      .pillHeight()
+    }
+    .buttonStyle(SearchFiltersPillStyle(
+      isHighlighted: self.isHighlighted
+    ))
+  }
+}
